a9054ef1d903a27a987c68cfdec8a11b1b91fde2
Kingsley Ijomah <kingsley@kocolondon.com>
2013-11-18 21:55:37 +0000

    strip to basic

--- rubywarrior/ratman-beginner/player.rb
+++ rubywarrior/ratman-beginner/player.rb
@@ -3,21 +3,21 @@ class Player
   	@health
 
   	unless warrior.feel.enemy?
-  		if warrior.feel.to_s == 'nothing'
-  			@health ||= warrior.health
-  			if warrior.health < 20
-  				if warrior.health < @health
-  					warrior.walk!
-	  				@health = warrior.health
-	  			else
-	  				warrior.rest!
-	  			end
-	  		else
-	  			warrior.walk!
-	  		end
-  		else warrior.feel.to_s == 'Captive'
-  			warrior.rescue!
-  		end
+  		# if warrior.feel.to_s == 'nothing'
+  		# 	@health ||= warrior.health
+  		# 	if warrior.health < 20
+  		# 		if warrior.health < @health
+  		# 			warrior.walk!
+	  	# 			@health = warrior.health
+	  	# 		else
+	  	# 			warrior.rest!
+	  	# 		end
+	  	# 	else
+	  	# 		warrior.walk!
+	  	# 	end
+  		# else warrior.feel.to_s == 'Captive'
+  		# 	warrior.rescue!
+  		# end
   	end
   	warrior.attack! if warrior.feel.enemy?
   	@health = warrior.health
