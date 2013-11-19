0923815dc2ef34acebe169f0096359d8345e18cf
Kingsley Ijomah <kingsley@kocolondon.com>
2013-11-19 20:50:05 +0000

    Start implementing back walk

--- rubywarrior/ratman-beginner/player.rb
+++ rubywarrior/ratman-beginner/player.rb
@@ -1,24 +1,35 @@
 class Player
   def play_turn(warrior)
   	@health
+    @from_start = false
+    unless @from_start
+      # if warrior.walk(:backward)
+      #   if warrior.feel.to_s == 'Captive'
+      #     warrior.rescue!
+      #   end
+      # end
+      # warrior.walk! :backward
 
-  	unless warrior.feel.enemy?
-  		# if warrior.feel.to_s == 'nothing'
-  		# 	@health ||= warrior.health
-  		# 	if warrior.health < 20
-  		# 		if warrior.health < @health
-  		# 			warrior.walk!
-	  	# 			@health = warrior.health
-	  	# 		else
-	  	# 			warrior.rest!
-	  	# 		end
-	  	# 	else
-	  	# 		warrior.walk!
-	  	# 	end
-  		# else warrior.feel.to_s == 'Captive'
-  		# 	warrior.rescue!
-  		# end
-  	end
+    else
+      @from_start = true
+    	unless warrior.feel.enemy?
+    		if warrior.feel.to_s == 'nothing'
+    			@health ||= warrior.health
+    			if warrior.health < 20
+    				if warrior.health < @health
+    					warrior.walk!
+  	  				@health = warrior.health
+  	  			else
+  	  				warrior.rest!
+  	  			end
+  	  		else
+  	  			warrior.walk!
+  	  		end
+    		else warrior.feel.to_s == 'Captive'
+    			warrior.rescue!
+    		end
+    	end
+    end
   	warrior.attack! if warrior.feel.enemy?
   	@health = warrior.health
   end
